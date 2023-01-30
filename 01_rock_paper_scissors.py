fe7b11c3f0c0cbc3a85a537f980ab29c73e7758d
Peter Hardy <peterphardy@gmail.com>
2023-01-29 22:23:32 -0600

    refacotr with while loop to play again

--- 01_rock_paper_scissors.py
+++ 01_rock_paper_scissors.py
@@ -1,16 +1,16 @@
 from random import randint 
 
-play_again: bool = True
+player_wins: int = 0
+computer_wins: int = 0
 
-while play_again:
-    play_again: bool = False
+while player_wins < 2 and computer_wins < 2:
+    print(f"Player Score: {player_wins} Computer Score: {computer_wins}")
     print("Rock...")
     print("Paper...")
     print("Scissors...")
     print("Shoot!")
 
     player_1: str = input("Player, make your move: ").lower()
-
     random_choice: int = randint(0,2)
 
     if random_choice == 0:
@@ -27,22 +27,24 @@ while play_again:
     elif player_1 == "rock":
         if computer == "scissors":
             print(f"{player_1.capitalize()} beats {computer}, Player wins!")
+            player_wins += 1
         elif computer == "paper":
             print(f"{computer.capitalize()} beats {player_1}, Computer wins!")
+            computer_wins += 1
     elif player_1 == "paper":
         if computer == "rock":
             print(f"{player_1.capitalize()} beats {computer}, Player wins!")
+            player_wins += 1
         elif computer == "scissors":
             print(f"{computer.capitalize()} beats {player_1}, Computer wins!")
+            computer_wins += 1
     elif player_1 == "scissors":
         if computer == "paper":
             print(f"{player_1.capitalize()} beats {computer}, Player wins!")
+            player_wins += 1
         elif computer == "rock":
             print(f"{computer.capitalize()} beats {player_1}, Computer wins!")
+            computer_wins += 1
     else:
         print("You are playing the game wrong. Make a valid move silly!")
-
-    new_game: bool = input("Would you like to play again? If so enter 'yes' ").lower()
-
-    if new_game == "yes":
-        play_again = True
+print(f"FINAL SCORES... Player: {player_wins} Computer: {computer_wins}")
