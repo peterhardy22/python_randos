540dd9c3d0656c4a8a4b2b1e7b7811c48956d8cc
Peter Hardy <peterphardy@gmail.com>
2023-02-11 21:38:22 -0600

    dictionaries for functions

--- functions.py
+++ functions.py
@@ -10,3 +10,15 @@ def math(a: int, b: int, func=add) -> int:
 
 # math(2, 2)
 # math(2, 2, subtract)
+
+# Using dictionaries instead of long conditional.
+def speak(animal="dog"):
+    noises = {"dog": "woof", "pig": "oink", "duck": "quack", "cat": "meow"}
+    noise = noises.get(animal)
+    if noise:
+        return noise
+    return "?"
+
+def speak(animal='dog'):
+    noises = {'pig':'oink', 'duck':'quack', 'cat':'meow', 'dog':'woof'}
+    return noises.get(animal, '?')
