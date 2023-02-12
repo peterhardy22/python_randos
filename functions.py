a5a4a47fb8761a99d5269ffc6e6c788a7908e6ba
Peter Hardy <peterphardy@gmail.com>
2023-02-11 21:53:39 -0600

    printing out functions comments

--- functions.py
+++ functions.py
@@ -6,10 +6,11 @@ def subtract(a: int, b: int) -> int:
     return a - b
 
 def math(a: int, b: int, func=add) -> int:
+    """Function uses a function as a parameter"""
     return func(a, b)
 
-# math(2, 2)
-# math(2, 2, subtract)
+# print(math(2, 2))
+# print(math(2, 2, subtract))
 
 
 # Using dictionaries instead of long conditional.
@@ -21,6 +22,7 @@ def speak(animal="dog"):
     return "?"
 
 def speak(animal='dog'):
+    """"Example of using default argument in dict.get()"""
     noises = {'pig':'oink', 'duck':'quack', 'cat':'meow', 'dog':'woof'}
     return noises.get(animal, '?')
 
@@ -29,15 +31,21 @@ def speak(animal='dog'):
 total: int = 0
 
 def increment() -> int:
+    """Example of using a variable in a function that is global"""
     global total
     total += 1
     return total
 
 # Nonlocal scope.
 def outer() -> int:
+    """Example of using a variable with nonlocal scope"""
     count: int = 0
     def inner() -> int:
         nonlocal count
         count += 1
         return count
     return inner()
+
+# Retrieves a functions """ comments!
+# function_name.__doc__
+print(outer.__doc__)
