f44119bee047a46f1edbaab14ca1b9b0deb586c9
Peter Hardy <peterphardy@gmail.com>
2023-02-12 21:17:03 -0600

    Good example of dictionary comprehension

--- functions.py
+++ functions.py
@@ -49,3 +49,11 @@ def outer() -> int:
 # Retrieves a functions """ comments!
 # function_name.__doc__
 print(outer.__doc__)
+
+
+# flesh out multiple_letter count:
+def multiple_letter_count(string):
+    return {letter: string.count(letter) for letter in string}
+
+# multiple_letter_count("awesome") # {'a': 1, 'e': 2, 'm': 1, 'o': 1, 's': 1, 'w': 1}
+
