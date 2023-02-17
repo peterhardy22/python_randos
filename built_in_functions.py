3a719ccd50b6756b7f0b62a431df257358baf3ee
Peter Hardy <peterphardy@gmail.com>
2023-02-16 22:06:07 -0600

    use of filter in a function

--- built_in_functions.py
+++ built_in_functions.py
@@ -7,3 +7,11 @@ def decrement_list(numbers: list) -> list:
     """Example function displaying use of map to take in a list aand decrement it by 1
     returning the list."""
     return list(map(lambda number: number - 1, numbers))
+
+
+# Combing Map and Filter.
+names: list = ["Peter", "Cassie", "Mickie", "Josie"]
+
+example = list(map(lambda name: f"Your friend today is {name}",
+            filter(lambda value: len(value) < 6, names)))
+# print(example)
