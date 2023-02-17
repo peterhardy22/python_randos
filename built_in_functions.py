c7c4fc1f7d30cbce9db1af1a14c9e5702a578093
Peter Hardy <peterphardy@gmail.com>
2023-02-16 21:56:22 -0600

    Map in a function example

--- built_in_functions.py
+++ built_in_functions.py
@@ -2,4 +2,8 @@
 cube = lambda num: num ** 3
 
 
-#
+# Map.
+def decrement_list(numbers: list) -> list:
+    """Example function displaying use of map to take in a list aand decrement it by 1
+    returning the list."""
+    return list(map(lambda number: number - 1, numbers))
