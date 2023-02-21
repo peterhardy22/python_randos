c0195a25ce3df11f0df1cd41742063aed710a30e
Peter Hardy <peterphardy@gmail.com>
2023-02-21 12:54:25 -0600

    Better use case of else in try and except

--- debugging.py
+++ debugging.py
@@ -13,3 +13,27 @@ while True:
         break
     finally:
         print("Runs every time regardless of a break in the else!")
+
+
+# def divide(a: int, b: int):
+#     try:
+#         result = a/b
+#     except ZeroDivisionError:
+#         print("You cannot divide by zero.")
+#     except TypeError:
+#         print("Both inputs must be integers or floats.")
+#     else:
+#         print(f"{result}")
+
+def divide(a: int, b: int):
+    try:
+        result = a/b
+    except (ZeroDivisionError, TypeError) as error:
+        print("Something went wrong.")
+        print(error)
+    else:
+        print(f"{result}")
+
+divide(1,2)
+divide(1,0)
+divide("a",2)
