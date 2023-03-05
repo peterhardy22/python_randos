10209979ec25877135de1ae23d1fea4774ac02b1
Peter Hardy <peterphardy@gmail.com>
2023-03-04 22:11:35 -0600

    init method

--- oop.py
+++ oop.py
@@ -1,4 +1,7 @@
 class User:
-    pass
+    def __init__(self, first, last, age) -> None:
+        self.first = first
+        self.last = last
+        self.age = age
 
-user1 = User()
+user1 = User("Peter", "Hardy", 34)
