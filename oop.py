a8e3e5471d4c95f4cfa116e648bc87d1bf0618c0
Peter Hardy <peterphardy@gmail.com>
2023-03-05 22:38:40 -0600

    Added class method to User

--- oop.py
+++ oop.py
@@ -1,31 +1,30 @@
-# class User:
-#     active_users: int = 0
+class User:
+    active_users: int = 0
 
-#     def __init__(self, first, last, age) -> None:
-#         self.first = first
-#         self.last = last
-#         self.age = age
-#         User.active_users += 1
+    def __init__(self, first, last, age) -> None:
+        self.first = first
+        self.last = last
+        self.age = age
+        User.active_users += 1
     
-#     def logout(self) -> str:
-#         """Subtracts a user from active_users."""
-#         User.active_users -= 1
-#         return f"{self.first} had logged out."
-
-#     def full_name(self) -> str:
-#         """Returns first and last name of user."""
-#         return f"{self.first} {self.last}"
+    @classmethod
+    def display_active_users(cls):
+        return f"There are currently {cls.active_users} active users."
     
-#     def initials(self) -> str:
-#         """Returns first intiial of users full name."""
-#         return f"{self.first[0]}.{self.last[0]}."
+    def logout(self) -> str:
+        """Subtracts a user from active_users."""
+        User.active_users -= 1
+        return f"{self.first} had logged out."
 
-# user1 = User("Peter", "Hardy", 34)
-# print(user1.full_name())
-# print(user1.initials())
+    def full_name(self) -> str:
+        """Returns first and last name of user."""
+        return f"{self.first} {self.last}"
+    
+    def initials(self) -> str:
+        """Returns first intiial of users full name."""
+        return f"{self.first[0]}.{self.last[0]}."
 
 
-# # Define Bank Account Below:
 # class BankAccount:
 #     def __init__(self, owner: str, balance: float = 0.0) -> None:
 #         self.owner: str = owner
@@ -42,32 +41,34 @@
 #         return self.balance
 
 
-class Pet:
-    allowed_animals: list = ["cat", "dog", "fish", "rat"]
+# class Pet:
+#     allowed_animals: list = ["cat", "dog", "fish", "rat"]
 
-    def __init__(self, name: str, species: str) -> None:
-        if species not in Pet.allowed_animals:
-            raise ValueError(f"You can't have a {species} pet!")
-        self.name: str = name
-        self.species: str = species
+#     def __init__(self, name: str, species: str) -> None:
+#         if species not in Pet.allowed_animals:
+#             raise ValueError(f"You can't have a {species} pet!")
+#         self.name: str = name
+#         self.species: str = species
     
-    def set_species(self, species: str) -> str:
-        if species not in Pet.allowed_animals:
-            raise ValueError(f"You can't have a {species} pet!")
-        self.species = species
-        return f"This animals new species is {species}!"
+#     def set_species(self, species: str) -> str:
+#         if species not in Pet.allowed_animals:
+#             raise ValueError(f"You can't have a {species} pet!")
+#         self.species = species
+#         return f"This animals new species is {species}!"
 
 
-class Chicken:
-    total_eggs: int = 0
+# class Chicken:
+#     total_eggs: int = 0
     
-    def __init__(self, species: str, name: str, eggs: int = 0) -> None:
-        self.species: str = species
-        self.name: str = name
-        self.eggs: int = eggs
+#     def __init__(self, species: str, name: str, eggs: int = 0) -> None:
+#         self.species: str = species
+#         self.name: str = name
+#         self.eggs: int = eggs
         
-    def lay_egg(self) -> int:
-        """Keeps track of how many individual and collective eggs are being laid."""
-        self.eggs += 1
-        Chicken.total_eggs += 1
-        return self.eggs
+#     def lay_egg(self) -> int:
+#         """Keeps track of how many individual and collective eggs are being laid."""
+#         self.eggs += 1
+#         Chicken.total_eggs += 1
+#         return self.eggs
+
+
