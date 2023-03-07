a1cfec387830c73fc0bba429ae6120008d0ac2a8
Peter Hardy <peterphardy@gmail.com>
2023-03-06 22:55:55 -0600

    Ooop

--- oop.py
+++ oop.py
@@ -32,50 +32,50 @@ class User:
         return f"{self.first[0]}.{self.last[0]}."
 
 
-# class BankAccount:
-#     def __init__(self, owner: str, balance: float = 0.0) -> None:
-#         self.owner: str = owner
-#         self.balance: float = balance
+class BankAccount:
+    def __init__(self, owner: str, balance: float = 0.0) -> None:
+        self.owner: str = owner
+        self.balance: float = balance
     
-#     def deposit(self, amount: float) -> float:
-#         """Adds provided amount to balance."""
-#         self.balance += amount
-#         return self.balance
+    def deposit(self, amount: float) -> float:
+        """Adds provided amount to balance."""
+        self.balance += amount
+        return self.balance
         
-#     def withdraw(self, amount: float) -> float:
-#         """Subtracts provided amount from balance."""
-#         self.balance -= amount
-#         return self.balance
+    def withdraw(self, amount: float) -> float:
+        """Subtracts provided amount from balance."""
+        self.balance -= amount
+        return self.balance
 
 
-# class Pet:
-#     allowed_animals: list = ["cat", "dog", "fish", "rat"]
+class Pet:
+    allowed_animals: list = ["cat", "dog", "fish", "rat"]
 
-#     def __init__(self, name: str, species: str) -> None:
-#         if species not in Pet.allowed_animals:
-#             raise ValueError(f"You can't have a {species} pet!")
-#         self.name: str = name
-#         self.species: str = species
+    def __init__(self, name: str, species: str) -> None:
+        if species not in Pet.allowed_animals:
+            raise ValueError(f"You can't have a {species} pet!")
+        self.name: str = name
+        self.species: str = species
     
-#     def set_species(self, species: str) -> str:
-#         if species not in Pet.allowed_animals:
-#             raise ValueError(f"You can't have a {species} pet!")
-#         self.species = species
-#         return f"This animals new species is {species}!"
+    def set_species(self, species: str) -> str:
+        if species not in Pet.allowed_animals:
+            raise ValueError(f"You can't have a {species} pet!")
+        self.species = species
+        return f"This animals new species is {species}!"
 
 
-# class Chicken:
-#     total_eggs: int = 0
+class Chicken:
+    total_eggs: int = 0
     
-#     def __init__(self, species: str, name: str, eggs: int = 0) -> None:
-#         self.species: str = species
-#         self.name: str = name
-#         self.eggs: int = eggs
+    def __init__(self, species: str, name: str, eggs: int = 0) -> None:
+        self.species: str = species
+        self.name: str = name
+        self.eggs: int = eggs
         
-#     def lay_egg(self) -> int:
-#         """Keeps track of how many individual and collective eggs are being laid."""
-#         self.eggs += 1
-#         Chicken.total_eggs += 1
-#         return self.eggs
+    def lay_egg(self) -> int:
+        """Keeps track of how many individual and collective eggs are being laid."""
+        self.eggs += 1
+        Chicken.total_eggs += 1
+        return self.eggs
 
 
