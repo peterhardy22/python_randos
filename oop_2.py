4480ea05d696322a7b52999c4d6634c3a3b5ce38
Peter Hardy <peterphardy@gmail.com>
2023-03-07 21:13:07 -0600

    super() being super

--- oop_2.py
+++ oop_2.py
@@ -1,9 +1,25 @@
 # Inheritance.
 class Animal:
-    cool: bool = True
+    def __init__(self, name: str, species: str) -> None:
+        self.name: str = name
+        self.species: str = species
+    
+    def __repr__(self) -> str:
+        return f"{self.name} is a {self.species}."
 
     def make_sound(self, sound: str) -> str:
-        print(f"This animal says {sound}!")
+        """Returns the sound the animal makes based on the argument passed in."""
+        print(f"This animal named {self.name} says {sound}!")
 
 class Cat(Animal):
-    pass
+    def __init__(self, name: str, breed: str, toy: str) -> None:
+        super().__init__(name, species="Cat")
+        self.breed: str = breed
+        self.toy: str = toy
+    
+    def play(self) -> str:
+        print(f"{self.name} plays with {self.toy}!")
+
+charlie = Cat("Charlie", "Russian Blue", "String")
+print(charlie)
+charlie.play()
