6fe10edc961418d63e340724f4c1977cb47569d1
Peter Hardy <peterphardy@gmail.com>
2023-03-11 21:44:41 -0600

    Beats function

--- iterators_generators.py
+++ iterators_generators.py
@@ -17,3 +17,16 @@ def yes_or_no() -> str:
     while True:
         yield answer
         answer = "no" if answer == "yes" else "yes"
+
+
+def current_beat() -> int:
+    """
+    Returns a standard beat count infinitly.
+    """
+    numbers: tuple = (1, 2, 3, 4)
+    index: int = 0
+
+    while True:
+        if index >= len(numbers): index = 0
+        yield numbers[index]
+        index += 1
