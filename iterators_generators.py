2395645b196e319515b5ebe3aa431001e4d97f69
Peter Hardy <peterphardy@gmail.com>
2023-03-11 21:36:39 -0600

    generator exercises

--- iterators_generators.py
+++ iterators_generators.py
@@ -1,6 +1,19 @@
 def week() -> str:
+    """
+    Returns a day of the week until the end of the week.
+    """
     days: list = ["Monday", "Tuesday", "Wednesday",
         "Thursday", "Friday", "Saturday", "Sunday"]
     
     for day in days:
         yield day 
+
+
+def yes_or_no() -> str:
+    """
+    Returns yes if no was the previous response and vice versa.
+    """
+    answer: str = "yes"
+    while True:
+        yield answer
+        answer = "no" if answer == "yes" else "yes"
