0b41849f5924e488cef722b9d7323554df37883a
Peter Hardy <peterphardy@gmail.com>
2023-03-14 22:36:05 -0500

    Decorator syntax for commenting

--- decorators.py
+++ decorators.py
@@ -1,8 +1,6 @@
 def shout(func: function) -> str:
     def wrapper(*args, **kwargs):
-        """
-        Wrapper function that returns string in uppercase.
-        """
+        """Wrapper function that returns string in uppercase."""
         return func(*args, **kwargs).upper()
     return wrapper
 
@@ -18,6 +16,28 @@ def order(main: str, side: str) -> str:
 def lol() -> str:
 	return "lol"
 
-print(greet("todd"))
-print(order(side="burger", main="fries"))
-print(lol())
+# print(greet("todd"))
+# print(order(side="burger", main="fries"))
+# print(lol())
+
+
+from functools import wraps
+
+def log_function_data(func: function) -> int:
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        """I am the wrapper function."""
+        print(f"you are about to call {func.__name__}")
+        print(f"Here's the documentation: {func.__doc__}")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@log_function_data
+def add(x: int, y: int) -> int:
+    """Adds two numbers together."""
+    return x + y
+
+print(add.__doc__)
+print(add.__name__)
+help(add)
