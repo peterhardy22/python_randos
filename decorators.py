a093a5fa680f1a0529adf8ce65be31b256fc7e6f
Peter Hardy <peterphardy@gmail.com>
2023-03-16 21:49:08 -0500

    Fewer than three args exercise complete

--- decorators.py
+++ decorators.py
@@ -84,3 +84,23 @@ def do_nothing(*args, **kwargs):
     pass
 
 # do_nothing(1, 2, 3,a="hi",b="bye")
+
+
+def ensure_fewer_than_three_args(func: function):
+    wraps(func)
+    def wrapper(*args, **kwargs):
+        if len(args) < 3:
+            return func(*args, **kwargs)
+        return "Too many arguments!"
+    return wrapper
+
+# @ensure_fewer_than_three_args
+# def add_all(*nums):
+#     return sum(nums)
+
+# add_all() # 0
+# add_all(1) # 1
+# add_all(1,2) # 3
+# add_all(1,2,3) # "Too many arguments!"
+# add_all(1,2,3,4,5,6) # "Too many arguments!"
+
