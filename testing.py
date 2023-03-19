92e7fcebbc4f44be1fa04b2a13135365ff213872
Peter Hardy <peterphardy@gmail.com>
2023-03-18 20:11:10 -0500

    added two doctests

--- testing.py
+++ testing.py
@@ -1,4 +1,12 @@
 def add_positive_numbers(x: int, y: int) -> int:
+    """
+    >>> add_positive_numbers(1, 1)
+    2
+    >>> add_positive_numbers(1, -1)
+    Traceback (most recent call last):
+    ...
+    AssertionError: Both numbers must be positive!
+    """
     assert x > 0 and y > 0, "Both numbers must be positive!"
     return x + y
 
