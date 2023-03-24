82c07ae280a6b6a838f4db5ddef9d101a2b6c854
Peter Hardy <peterphardy@gmail.com>
2023-03-24 15:27:30 -0500

    function to find users by name in a csv

--- csv_work.py
+++ csv_work.py
@@ -24,3 +24,19 @@ def print_users():
     with open("users.csv") as csvfile:
         csv_reader = csv.DictReader(csvfile)
         list(print(f"{row['First Name']} {row['Last Name']}") for row in csv_reader)
+
+
+'''
+find_user("Colt", "Steele") # 1
+find_user("Alan", "Turing") # 3
+find_user("Not", "Here") # 'Not Here not found.'
+'''
+def find_user(first_name: str, last_name: str):
+    with open("users.csv") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for (index, row) in enumerate(csv_reader):
+            first_name_match = first_name == row[0]
+            last_name_match = last_name == row[1]
+            if first_name_match and last_name_match:
+                return index
+        return f"{first_name} {last_name} not found."
