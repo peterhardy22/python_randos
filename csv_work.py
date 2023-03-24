ee1645567483d875753dff25c86840a37f1aeb5d
Peter Hardy <peterphardy@gmail.com>
2023-03-24 15:04:24 -0500

    printing dynamic rows exercise

--- csv_work.py
+++ csv_work.py
@@ -13,3 +13,14 @@ def add_user(first_name: str, last_name: str) -> None:
     with open("users.csv", "a") as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow([first_name, last_name])
+
+
+'''
+print_users() # None
+# prints to the console:
+# Colt Steele
+'''
+def print_users():
+    with open("users.csv") as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        list(print(f"{row['First Name']} {row['Last Name']}") for row in csv_reader)
