62fbfe4569dd1b0098ab11e8e726f72dbab235d8
Peter Hardy <peterphardy@gmail.com>
2023-03-26 14:33:14 -0500

    delete user function completed

--- csv_work.py
+++ csv_work.py
@@ -49,7 +49,7 @@ update_users("Grace", "Hopper", "Hello", "World") # Users updated: 1.
 update_users("Colt", "Steele", "Boba", "Fett") # Users updated: 2.
 update_users("Not", "Here", "Still not", "Here") # Users updated: 0.
 '''
-def update_users(old_first: str, old_last: str, new_first: str, new_last: str):
+def update_users(old_first: str, old_last: str, new_first: str, new_last: str) -> str:
     """"This function updates a users first and last name."""
     with open("users.csv") as csvfile:
         csv_reader = csv.reader(csvfile)
@@ -66,3 +66,27 @@ def update_users(old_first: str, old_last: str, new_first: str, new_last: str):
                 csv_writer.writerow(row)
  
     return f"Users updated: {count}."
+
+
+'''
+delete_users("Grace", "Hopper") # Users deleted: 1.
+delete_users("Colt", "Steele") # Users deleted: 2.
+delete_users("Not", "Here") # Users deleted: 0.
+'''
+
+def delete_users(first_name: str, last_name: str) -> str:
+    """This function can delete a user based on name."""
+    with open("users.csv") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        rows: list = list(csv_reader)
+ 
+    count: int = 0
+    with open("users.csv", "w") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        for row in rows:
+            if row[0] == first_name and row[1] == last_name:
+                count += 1
+            else:
+                csv_writer.writerow(row)
+ 
+    return f"Users deleted: {count}."
