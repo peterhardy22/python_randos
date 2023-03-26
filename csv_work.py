c67278a092e13a7597a3c5bb38b2232f246e37da
Peter Hardy <peterphardy@gmail.com>
2023-03-25 22:05:35 -0500

    update a user based on name

--- csv_work.py
+++ csv_work.py
@@ -21,6 +21,7 @@ print_users() # None
 # Colt Steele
 '''
 def print_users():
+    """This function reads users from a CSV file."""
     with open("users.csv") as csvfile:
         csv_reader = csv.DictReader(csvfile)
         list(print(f"{row['First Name']} {row['Last Name']}") for row in csv_reader)
@@ -32,6 +33,7 @@ find_user("Alan", "Turing") # 3
 find_user("Not", "Here") # 'Not Here not found.'
 '''
 def find_user(first_name: str, last_name: str):
+    """This function finds a user based on first and last name."""
     with open("users.csv") as csvfile:
         csv_reader = csv.reader(csvfile)
         for (index, row) in enumerate(csv_reader):
@@ -40,3 +42,27 @@ def find_user(first_name: str, last_name: str):
             if first_name_match and last_name_match:
                 return index
         return f"{first_name} {last_name} not found."
+    
+
+'''
+update_users("Grace", "Hopper", "Hello", "World") # Users updated: 1.
+update_users("Colt", "Steele", "Boba", "Fett") # Users updated: 2.
+update_users("Not", "Here", "Still not", "Here") # Users updated: 0.
+'''
+def update_users(old_first: str, old_last: str, new_first: str, new_last: str):
+    """"This function updates a users first and last name."""
+    with open("users.csv") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        rows: list = list(csv_reader)
+ 
+    count: int = 0
+    with open("users.csv", "w") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        for row in rows:
+            if row[0] == old_first and row[1] == old_last:
+                csv_writer.writerow([new_first, new_last])
+                count += 1
+            else:
+                csv_writer.writerow(row)
+ 
+    return f"Users updated: {count}."
