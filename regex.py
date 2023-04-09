3dbae95a39ab80cef37bb142eabf3c45bc3c0d19
Peter Hardy <peterphardy@gmail.com>
2023-04-08 22:19:01 -0500

    import re module

--- regex.py
+++ regex.py
@@ -1,3 +1,10 @@
-# Email -> ^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-z0-9-.]+$
+import re
 
-# Regex examples to come...
+
+email: str = "duder@dude.com"
+result = re.search("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-z0-9-.]+$", email)
+
+if result:
+  print("YES! We have an email address!")
+else:
+  print("Not a valid email address!")
