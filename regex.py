83b08e44384e6203df4b2b58981c9c886656920f
Peter Hardy <peterphardy@gmail.com>
2023-04-09 22:24:36 -0500

    regex for phone number

--- regex.py
+++ regex.py
@@ -1,10 +1,15 @@
 import re
 
 
-email: str = "duder@dude.com"
-result = re.search("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-z0-9-.]+$", email)
+# Regex for an email.
+# email: str = "duder@dude.com"
+# result = re.search("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-z0-9-.]+$", email)
+# if result:
+#   print("YES! We have an email address!")
+# else:
+#   print("Not a valid email address!")
 
-if result:
-  print("YES! We have an email address!")
-else:
-  print("Not a valid email address!")
+
+# Regex for a phone number.
+phone_pattern = re.compile(r'\d{3} \d{3}-\d{4}')
+result = phone_pattern.search('Call me at 415 555-4242!')
