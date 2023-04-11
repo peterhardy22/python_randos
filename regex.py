c61691fbfde222ecc356bfc15bbca9eab304aedc
Peter Hardy <peterphardy@gmail.com>
2023-04-10 21:50:12 -0500

    function for extracting phone numbers

--- regex.py
+++ regex.py
@@ -11,11 +11,22 @@ import re
 
 
 # Regex for a phone number.
-phone_pattern = re.compile(r"\d{3} \d{3}-\d{4}")
-result_1 = phone_pattern.search("Call me at 415 555-4242!")
-result_2: list = phone_pattern.findall("Call me at 768 789-9845 or 634 523-2134")
-try:
-  print(result_1.group())
-except:
-  print("No phone number was found.")
-print(result_2)
+# phone_pattern = re.compile(r"\d{3} \d{3}-\d{4}")
+# result_1 = phone_pattern.search("Call me at 415 555-4242!")
+# result_2: list = phone_pattern.findall("Call me at 768 789-9845 or 634 523-2134")
+# try:
+#   print(result_1.group())
+# except:
+#   print("No phone number was found.")
+# print(result_2)
+
+def extract_phone(input: str):
+    """Extracts all phone numbers found in a string."""
+    phone_regex = re.compile(r"\b\d{3} \d{3}-\d{4}\b")
+    match = phone_regex.search(input)
+    if match:
+      return match.group()
+    return None
+
+# def is_valid_phone():
+    # """Checks if an entire string is a valid phone number."""
