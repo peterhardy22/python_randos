3aee2449677d97a971c8f9b1cc70aafa2e49b583
Peter Hardy <peterphardy@gmail.com>
2023-04-11 21:34:49 -0500

    odd indexing of group

--- regex.py
+++ regex.py
@@ -56,5 +56,9 @@ def is_valid_time(input: str) -> bool:
 
 url_regex = re.compile(r"(https?)://(www\.[A-za-z-]{2,256}\.[a-z]{2,6})([-a-zA-Z0-9@:%_\+.~#?&//=]*)")
 match = url_regex.search("http://www.youtube.com/videos/abc123")
-print(match.group())
-print(match.groups())
+# print(match.group())
+# print(match.groups())
+print(match.group(0))
+print(match.group(1))
+print(match.group(2))
+print(match.group(3))
