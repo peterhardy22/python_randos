edfd09326861ea7ebf5e3acff9fa566501dd174f
Peter Hardy <peterphardy@gmail.com>
2023-04-11 21:36:57 -0500

    parsing bytes exercise complete

--- regex.py
+++ regex.py
@@ -58,7 +58,14 @@ url_regex = re.compile(r"(https?)://(www\.[A-za-z-]{2,256}\.[a-z]{2,6})([-a-zA-Z
 match = url_regex.search("http://www.youtube.com/videos/abc123")
 # print(match.group())
 # print(match.groups())
-print(match.group(0))
-print(match.group(1))
-print(match.group(2))
-print(match.group(3))
+# print(match.group(0))
+# print(match.group(1))
+# print(match.group(2))
+# print(match.group(3))
+
+
+def parse_bytes(input: str) -> list:
+    """Returns a list of the binary bytes contained in the input string."""
+    binary_regex = re.compile(r'\b[10]{8}\b')
+    results: list = binary_regex.findall(input)
+    return results
