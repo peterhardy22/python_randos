bdd4d1f89b5b107f1ccad46c3cdf3e12da4c9028
Peter Hardy <peterphardy@gmail.com>
2023-04-18 21:24:51 -0500

    added item to table

--- sql.py
+++ sql.py
@@ -3,7 +3,13 @@ import sqlite3
 
 conn = sqlite3.connect("my_friends.db")
 cursor = conn.cursor()
-cursor.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
+
+# Creates table.
+# cursor.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
+
+insert_query = '''INSERT INTO friends 
+                    VALUES ('Harrison', 'George', 64)'''
+cursor.execute(insert_query)
 conn.commit()
 conn.close()
 
@@ -11,4 +17,5 @@ conn.close()
 # sqlite3 {database.db}
 # .tables
 # .schema
+# select * from table;
 # .quit
