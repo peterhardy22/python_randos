b3e19cf014b8a98a6603b816636b80d27085276b
Peter Hardy <peterphardy@gmail.com>
2023-04-18 21:19:49 -0500

    executed sql statement thru python

--- sql.py
+++ sql.py
@@ -1,8 +1,14 @@
-# sqlite3
-# .quit
-
 import sqlite3
 
-conn = sqlite3.connect("my_friends.db")
 
+conn = sqlite3.connect("my_friends.db")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
+conn.commit()
 conn.close()
+
+
+# sqlite3 {database.db}
+# .tables
+# .schema
+# .quit
