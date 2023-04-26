6ed43fd0dfd738761718425c22e31a5b69ca219a
Peter Hardy <peterphardy@gmail.com>
2023-04-25 22:01:09 -0500

    Challenge 109

--- extra_challenges.py
+++ extra_challenges.py
@@ -7,3 +7,14 @@ reverse_string('Elie') # 'eilE'
 def reverse_string(word: str) -> str:
     """Reverses given word."""
     return word[::-1]
+
+
+'''
+list_check([[],[1],[2,3], (1,2)]) # False
+list_check([1, True, [],[1],[2,3]]) # False
+list_check([[],[1],[2,3]]) # True
+'''
+
+def list_check(input_list: list) -> bool:
+    """Determines if all elements in the given list are of the datatype list."""
+    return all((isinstance(element, list) for element in input_list))
