73dcb6a9da5b5577f709f6eca7d128a2aca5ee55
Peter Hardy <peterphardy@gmail.com>
2023-04-25 22:14:40 -0500

    Challenge 110

--- extra_challenges.py
+++ extra_challenges.py
@@ -18,3 +18,14 @@ list_check([[],[1],[2,3]]) # True
 def list_check(input_list: list) -> bool:
     """Determines if all elements in the given list are of the datatype list."""
     return all((isinstance(element, list) for element in input_list))
+
+
+'''
+remove_every_other([1,2,3,4,5]) # [1,3,5] 
+remove_every_other([5,1,2,4,1]) # [5,2,1]
+remove_every_other([1]) # [1] 
+'''
+
+def remove_every_other(input_list: list) -> list:
+    """Returns the given lists' even number indexed items."""
+    return [number for index, number in enumerate(input_list) if index % 2 == 0]
