336c8a726a6e86b69b0ca786d9402e77c626dce9
Peter Hardy <peterphardy@gmail.com>
2023-05-07 13:38:39 -0500

    Three odd numbers challenge completed

--- extra_challenges.py
+++ extra_challenges.py
@@ -335,3 +335,23 @@ def valid_parentheses(paren_string: str) -> bool:
         iterator += 1
         
     return counter == 0
+
+
+'''
+three_odd_numbers([1,2,3,4,5]) # True
+three_odd_numbers([0,-2,4,1,9,12,4,1,0]) # True
+three_odd_numbers([5,2,1]) # False
+three_odd_numbers([1,2,3,3,2]) # False
+'''
+
+def three_odd_numbers(numbers_list: list) -> bool:
+    """Accepts a list of numbers and returns True if any 3 consecutive numbers sum to an odd number."""
+    for index, _ in enumerate(numbers_list):
+        try:
+            if sum([numbers_list[index], numbers_list[index+1], numbers_list[index+2]]) % 2 == 1:
+                return True
+        except:
+            pass
+    return False
+
+
