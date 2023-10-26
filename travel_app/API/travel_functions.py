13c1c1e466fb42d6eeca9eeb7561adad47495003
Peter Hardy <peterphardy@gmail.com>
2023-10-25 22:34:29 -0500

    Trying to use the Amadeus SDK

---
+++ travel_app/API/travel_functions.py
@@ -0,0 +1,29 @@
+from amadeus import Client, ResponseError
+import json
+import requests
+from API.config import base_url, token_headers, token_url
+from API.creds import api_key, api_secret
+
+
+def api_login():
+    """This function logs into the API and returns a token."""
+    amadeus = Client(
+        client_id=api_key,
+        client_secret=api_secret
+    )
+
+    try:
+        response = amadeus.shopping.flight_dates.get(origin='ORD', destination='LAX')
+        print(response.data)
+    except ResponseError as error:
+        print(error)
+
+
+def retrieve_travel_info(source, destination, start_date, end_date, headers):
+    """This functions calls the API and returns flight information based on the arguments."""
+    pass
+    
+
+def list_trip_options(api_response):
+    """Function which parses API response and returns top 5 list of airfare choices based on price."""
+    pass
---
+++ travel_app/main.py
@@ -0,0 +1,11 @@
+from API.travel_functions import api_login, retrieve_travel_info
+
+
+# DISCLAIMER
+# Need to create a creds.py file within the API folder with the following keys and your own unique values
+# api_key: ADD YOUR API KEY HERE
+# api_secret: ADD YOUR API SECRET
+
+
+if __name__ == "__main__":
+    api_login()
