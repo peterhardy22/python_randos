5b0d306f3bab565f8a0194a16d9613385c36877e
Peter Hardy <peterphardy@gmail.com>
2023-10-30 21:22:17 -0500

    open, seek, read, readline, readlines, close

--- first_friday_two/edit_this.py
+++ first_friday_two/edit_this.py
@@ -1,2 +1,22 @@
-# open() allows you to open a file and return a corresponding file object.
-# Multiple optional parameters, but the name of the file is mandatory.
+# # open() is a built-in Python function (no imports!)
+# # Allows you to open a file and return a corresponding file object.
+# # Multiple optional parameters, but the name of the file is mandatory.
+file = open("starter.txt")
+# print(file) 
+# print(file.read())
+
+# seek
+# file = open("starter.txt")
+# print(file.read())
+# print(file.read())
+# file.seek(0)
+# print(file.read())
+
+# readline()
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+
+# print(file.readlines())
+
+file.close()
