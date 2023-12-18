5853bb68367c24396d7c1cda5ddad6026937f1fc
Peter Hardy <peterphardy@gmail.com>
2023-12-18 11:35:51 -0600

    Formatted and timestamped logging

--- logging_practice.py
+++ logging_practice.py
@@ -2,7 +2,11 @@ import logging
 
 
 def main() -> None:
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
     logging.debug("This is a debug message.")
     logging.info("This is an info message.")
