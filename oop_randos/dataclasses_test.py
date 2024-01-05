275a4241df85bb2d4a79fa53c7d21b6f220c4714
Peter Hardy <peterphardy@gmail.com>
2024-01-05 15:21:02 -0600

    Frozen dataclasses are read only

--- oop_randos/dataclasses_test.py
+++ oop_randos/dataclasses_test.py
@@ -1,23 +1,27 @@
 """
-    - order=True allows you to be able to compare objects (ex. person1>person2).
-        - sort_index: int allows you to sort on an int such as age.
-        - import field and field(init=False, repr=False) make it so it does not need to be initialized nor shown when printed.
+    - order=True - Allows you to be able to compare objects (ex. person1>person2).
+        - sort_index: int - Allows you to sort on an int such as age.
+        - import field and field(init=False, repr=False) - Make it so it does not need to be initialized nor shown when printed.
+    - frozen=True - Creates objects that are read only.
+        - object.__setattr__(self, "sort_index", self.strength) - Circumvents the dataclass being frozen.
+        
 """
 
 from dataclasses import dataclass, field
 
 
-@dataclass(order=True)
+@dataclass(order=True, frozen=True)
 class Person:
     sort_index: int = field(init=False, repr=False)
     name: str
     job: str
     age: int
+    strength: int = 100
 
     def __post_init__(self):
-        self.sort_index = self.age
+        object.__setattr__(self, "sort_index", self.strength)
 
-person1 = Person("Mickie", "Orchestrator", 4)
+person1 = Person("Mickie", "Orchestrator", 4, 95)
 person2 = Person("Josie", "Free Spirit", 3)
 person3 = Person("Josie", "Free Spirit", 3)
 person4 = Person("Walker", "Plumper", 0)
@@ -26,4 +30,4 @@ print(id(person2))
 print(id(person3))
 print(person1)
 
-print(person1<person2)
+print(person1>person2)
