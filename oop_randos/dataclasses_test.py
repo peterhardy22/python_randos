5161628d7eae78efff8fa36e49420df34c924d2c
Peter Hardy <peterphardy@gmail.com>
2024-01-05 15:09:24 -0600

    Converted to a dataclass

--- oop_randos/dataclasses_test.py
+++ oop_randos/dataclasses_test.py
@@ -1,12 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Person:
     name: str
     job: str
     age: int
-
-    def __init__(self, name, job, age) -> None:
-        self.name = name
-        self.job = job
-        self.age = age
 
 person1 = Person("Mickie", "Orchestrator", 4)
 person2 = Person("Josie", "Free Spirit", 3)
