c5a37ac189efbf16c5d8fc323758596dc422c776
Peter Hardy <peterphardy@gmail.com>
2024-01-05 15:15:53 -0600

    added ability to sort based on attribute for object comparison

--- oop_randos/dataclasses_test.py
+++ oop_randos/dataclasses_test.py
@@ -1,11 +1,21 @@
-from dataclasses import dataclass
+"""
+    - order=True allows you to be able to compare objects (ex. person1>person2).
+        - sort_index: int allows you to sort on an int such as age.
+        - import field and field(init=False, repr=False) make it so it does not need to be initialized nor shown when printed.
+"""
+
+from dataclasses import dataclass, field
 
 
-@dataclass
+@dataclass(order=True)
 class Person:
+    sort_index: int = field(init=False, repr=False)
     name: str
     job: str
     age: int
+
+    def __post_init__(self):
+        self.sort_index = self.age
 
 person1 = Person("Mickie", "Orchestrator", 4)
 person2 = Person("Josie", "Free Spirit", 3)
@@ -16,4 +26,4 @@ print(id(person2))
 print(id(person3))
 print(person1)
 
-print(person3 == person2)
+print(person1<person2)
