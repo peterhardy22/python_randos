37dc25b6973194dd8b33091a7f4fdec4754713c5
Peter Hardy <peterphardy@gmail.com>
2024-01-18 09:27:02 -0600

    Passing arguments in the function

--- randos_part_2/threading_practice.py
+++ randos_part_2/threading_practice.py
@@ -7,13 +7,13 @@ def print_names():
         print(name)
         time.sleep(random.uniform(0.5, 1.5))
 
-def print_age():
+def print_age(min_sleep: float, max_sleep: float):
     for _ in range(5):
         print(random.randint(20, 50))
-        time.sleep(random.uniform(0.5, 1.5))
+        time.sleep(random.uniform(min_sleep, max_sleep))
 
 t1 = threading.Thread(target=print_names)
-t2 = threading.Thread(target=print_age)
+t2 = threading.Thread(target=print_age, args=(0.2, 1))
 
 t1.start()
 t2.start()
