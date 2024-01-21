31cc76f17438d8dc53623181799843bd23ee8883
Peter Hardy <peterphardy@gmail.com>
2024-01-21 14:20:57 -0600

    simple multi-processing script

--- randos_part_2/multiprocessisng_practice.py
+++ randos_part_2/multiprocessisng_practice.py
@@ -1,12 +1,22 @@
-"""Tasked with trying to do mutliprocessing in a script."""
 import multiprocessing
+
 
 def square_sum(numbers: list) -> None:
     """Calculates the sum of squares of a list of numbers."""
     total: int = sum([num**2 for num in numbers])
     print(f"Sum of squares from {numbers=}: {total}")
 
-numbers: list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+if __name__ == '__main__':
+    numbers: list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
-chunk1 = numbers[:5]
-chunk2 = numbers[5:]
+    chunk1 = numbers[:5]
+    chunk2 = numbers[5:]
+
+    process1 = multiprocessing.Process(target=square_sum, args=(chunk1,))
+    process2 = multiprocessing.Process(target=square_sum, args=(chunk2,))
+
+    process1.start()
+    process2.start()
+
+    process1.join()
+    process2.join()
