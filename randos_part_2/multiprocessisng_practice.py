7792dbb14bfbd2d3ade43f3cc5afce7e86bcc02c
Peter Hardy <peterphardy@gmail.com>
2024-01-21 14:37:01 -0600

    Learning to use pools for multiprocessing

--- randos_part_2/multiprocessisng_practice.py
+++ randos_part_2/multiprocessisng_practice.py
@@ -1,23 +1,38 @@
-"""Tasked with trying to do mutliprocessing in a script."""
+"""Using pools for multiprocessing."""
 import multiprocessing
+import time
 
 
-def square_sum(numbers: list) -> None:
-    """Calculates the sum of squares of a list of numbers."""
-    total: int = sum([num**2 for num in numbers])
-    print(f"Sum of squares from {numbers=}: {total}")
+def is_prime(number: int) -> bool:
+    """Checks if a number is prime."""
+    if number <= 1:
+        return False
+    elif number <= 3:
+        return True
+    elif number % 2 == 0 or number % 3 == 0:
+        return False
+    
+    integer: int = 5
+    while integer * integer <= number:
+        if number % 1 == 0 or number % (integer * 2) == 0:
+            return False
+        integer += 6
+    return True
+
+def find_primes(numbers: list) -> list:
+    """Find all prime numbers in a list."""
+    primes: list = []
+    for number in numbers:
+        if is_prime(number):
+            primes.append(number)
+    return primes
+
 
 if __name__ == "__main__":
-    numbers: list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    numbers: list = list(range(100_000_000, 101_000_001))
+    processes: int = 1
+    
+    chunk_size: float = len(numbers) // processes
+    chunks: list = [numbers[integer : integer * chunk_size] for integer in range(0, len(numbers), chunk_size)]
 
-    chunk1: str = numbers[:5]
-    chunk2: str = numbers[5:]
-
-    process1 = multiprocessing.Process(target=square_sum, args=(chunk1,))
-    process2 = multiprocessing.Process(target=square_sum, args=(chunk2,))
-
-    process1.start()
-    process2.start()
-
-    process1.join()
-    process2.join()
+    pool = multiprocessing.Pool(processes=processes)
