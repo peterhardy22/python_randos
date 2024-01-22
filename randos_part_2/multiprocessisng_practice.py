5c07ebf698467beef524335c906f5136a8a4fffe
Peter Hardy <peterphardy@gmail.com>
2024-01-22 11:59:11 -0600

    Upping processes to a point speeds up the script

--- randos_part_2/multiprocessisng_practice.py
+++ randos_part_2/multiprocessisng_practice.py
@@ -14,7 +14,7 @@ def is_prime(number: int) -> bool:
     
     integer: int = 5
     while integer * integer <= number:
-        if number % 1 == 0 or number % (integer * 2) == 0:
+        if number % integer == 0 or number % (integer + 2) == 0:
             return False
         integer += 6
     return True
@@ -30,9 +30,27 @@ def find_primes(numbers: list) -> list:
 
 if __name__ == "__main__":
     numbers: list = list(range(100_000_000, 101_000_001))
-    processes: int = 1
+    processes: int = 5
     
     chunk_size: float = len(numbers) // processes
-    chunks: list = [numbers[integer : integer * chunk_size] for integer in range(0, len(numbers), chunk_size)]
+    chunks: list = [numbers[integer : integer + chunk_size] for integer in range(0, len(numbers), chunk_size)]
 
     pool = multiprocessing.Pool(processes=processes)
+    start_time = time.monotonic()
+
+    results = pool.map(find_primes, chunks)
+
+    primes: list = []
+    for result in results:
+        primes += result
+
+    pool.close()
+    pool.join()
+
+    end_time = time.monotonic()
+
+    print(
+        f"Found {len(primes):,} prime numbers "
+        f"between {numbers[0]:,} and {numbers[-1]:,} "
+        f"in {(end_time - start_time):.2f} seconds."
+    )
