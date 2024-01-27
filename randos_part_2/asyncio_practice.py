1d908800cdc2d10b7aa3f0b284b11a6a63da94e6
Peter Hardy <peterphardy@gmail.com>
2024-01-27 14:11:49 -0600

    Setting up practice to update this code

--- randos_part_2/asyncio_practice.py
+++ randos_part_2/asyncio_practice.py
@@ -1,2 +1,25 @@
 """Sandbox for experimenting with the asyncio import."""
-import asyncio
+import random
+import time
+import threading
+
+def print_names():
+    for name in ("Peter", "Cassie", "Mickie", "Josie", "Walker"):
+        print(name)
+        time.sleep(random.uniform(0.5, 1.5))
+
+def print_age(min_sleep, max_sleep):
+    for _ in range(5):
+        print(random.randint(20, 50))
+        time.sleep(random.uniform(min_sleep, max_sleep))
+
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target=print_names)
+    t2 = threading.Thread(target=print_age, args=(0.2, 1))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
